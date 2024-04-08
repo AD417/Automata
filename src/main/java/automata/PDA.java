9a1a14ca7f9fbefc074b5555e9ee83228cacdcf9
Alexander Day <55056986+AD417@users.noreply.github.com>
2024-04-07 23:32:58 -0400

    Document PDA fully

--- src/main/java/automata/PDA.java
+++ src/main/java/automata/PDA.java
@@ -7,15 +7,16 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 /**
- * A PushDown Automaton (PDA). Has identical functionality to a {@link NFA},
- * but includes a stack that may be manipulated to encode more complex
- * languages. PDAs are able to encode any
- * {@link grammar.CFG Context-Free Grammar}.
- * This PDA starts with an empty stack; there is no "starting symbol". To add
- * a starting symbol, encode a start state in the relation that pushes a start
- * symbol to the stack before going to the actual start of the PDA.
+ * A PushDown Automaton (PDA). Has comparable functionality to a {@link NFA},
+ * but includes more complex state manipulation to allow it to recognize a
+ * larger set of languages. <br>
+ * The main addition is a stack, a basic data structure allowing elements to
+ * be stored in a "Last In First Out" manner. The PDA can access the most
+ * recently pushed element on the stack, but must remove that element to
+ * access elements below it, if valid. <br>
+ * PDAs are able to encode any {@link grammar.CFG Context-Free Grammar}.
  * @param states The set of all states.
- * @param stringAlphabet The set of all symbols that can appear in the input
+ * @param stringAlphabet The set of all symbols that can be read from the input
  *                       string.
  * @param stackAlphabet The set of all symbols that can be pushed onto the
  *                      PDA stack.
@@ -28,6 +29,15 @@ import java.util.stream.Collectors;
  * @param acceptingStates The states that allow a PDA to accept a string if it
  *                        is possible for a set of moves to end up in one of
  *                        them. Must be a subset of the state set.
+ * @implNote some interpretations of the PDA's stack may include
+ * a "starting symbol". This implementation does not; to add a starting
+ * stack symbol, encode a start state in the relation that pushes the start
+ * symbol to the stack before continuing to the actual start of the PDA.
+ * Additionally, since it is possible for the PDA to get stuck in a loop of
+ * epsilon transitions that results in an infinite number of elements being
+ * pushed to the stack, it is assumed that a maximum of |Q| epsilon transitions
+ * may occur between reads from the tape head. This may be an underestimate in
+ * some cases, particularly when a PDA is created from a CFG.
  */
 public record PDA(Set<State> states,
                   Alphabet stringAlphabet,
@@ -36,16 +46,24 @@ public record PDA(Set<State> states,
                   State startState,
                   Set<State> acceptingStates) {
 
+    /**
+     * An internal hashable representation of the "true state" of a PDA at
+     * some time. Includes not only the current machine state, but the contents
+     * of the stack.
+     * @param state The current machine state.
+     * @param stack The contents of the stack.
+     */
     private record PDAConfiguration(State state, Stack<String> stack) {}
 
     /**
-     * The starting configuration, including the stack.
+     * The starting configuration, including the empty stack.
      * @return a configuration including the start state and empty stack.
      */
     private Set<PDAConfiguration> startConfig() {
         return epsilonClosure(new PDAConfiguration(startState, new Stack<>()));
     }
 
+    // Lazy method to clone and push/pop from the stack.
     private Stack<String> nextStack(Stack<String> current, String symbol, boolean isEpsilon) {
         @SuppressWarnings("unchecked")
         Stack<String> next = (Stack<String>) current.clone();
@@ -56,6 +74,14 @@ public record PDA(Set<State> states,
         return next;
     }
 
+    /**
+     * Perform a single transition for a given state, where you read the given
+     * symbol.
+     * @param config The configuration to transition.
+     * @param symbol The symbol the PDA read. May be epsilon.
+     * @return The set of all configurations reachable from
+     * this current configuration.
+     */
     private Set<PDAConfiguration> transitionStep(PDAConfiguration config, Character symbol) {
         Set<PDAConfiguration> step = new HashSet<>();
         if (symbol == Alphabet.EPSILON) step.add(config);
@@ -74,25 +100,19 @@ public record PDA(Set<State> states,
             }
         }
         return step;
-
-        /*Set<PDAConfiguration> epsilonStep = new HashSet<>(step);
-
-        for (PDAConfiguration pda : step) {
-            for (StackState result : relation.epsilonStackTransition(config.state, symbol)) {
-                Stack<String> futureStack = nextStack(config.stack, result.stackSymbol(), true);
-                epsilonStep.add(new PDAConfiguration(result.state(), futureStack));
-            }
-            if (!pda.stack.isEmpty()) {
-                StackState input = new StackState(pda.state, pda.stack.peek());
-                for (StackState result : relation.transition(input, symbol)) {
-                    Stack<String> futureStack = nextStack(pda.stack, result.stackSymbol(), false);
-                    epsilonStep.add(new PDAConfiguration(result.state(), futureStack));
-                }
-            }
-        }
-        return epsilonStep;*/
     }
 
+    /**
+     * Compute the Epsilon Closure of a configuration. This is an
+     * operation that determines the set of all reachable states and stack
+     * configurations from the input states via epsilon transitions alone.
+     * @param config A configuration to determine the epsilon closure of.
+     * @return The set of configs that can be reached via a reasonable number
+     * of epsilon transitions.
+     * @implNote "A reasonable number" of epsilon transitions is defined as the
+     * number of states in the PDA. This may not be a sufficient amount for
+     * some applications, such as a CFG-derived PDA.
+     */
     private Set<PDAConfiguration> epsilonClosure(PDAConfiguration config) {
         Set<PDAConfiguration> out = new HashSet<>();
         out.add(config);
@@ -101,11 +121,12 @@ public record PDA(Set<State> states,
         step.add(config);
         Set<PDAConfiguration> nextStep = new HashSet<>();
 
-        // This is a very hacky solution to ensure that the PDA never gets
-        // stuck in a loop while actually being able to do the epsilon
-        // transitions it needs to. It will do as many epsilon transitions
-        // as there are states in the machine. Arguably, this is also
-        // insufficient, but you can increase the states factor if necessary.
+        // NOTE:
+        //  This is a very hacky solution to ensure that the PDA never gets
+        //  stuck in a loop while actually being able to do the epsilon
+        //  transitions it needs to. It will do as many epsilon transitions
+        //  as there are states in the machine. Arguably, this is also
+        //  insufficient, but you can increase the states factor if necessary.
         for (int p = 0; p <= states().size(); p++) {
             nextStep = step.stream()
                     //.filter(out::contains)
@@ -119,8 +140,17 @@ public record PDA(Set<State> states,
         return out;
     }
 
+    /**
+     * Determine if a string is within the context-free language defined by
+     * this PDA. More specifically, determines if there is any possible set of
+     * steps that the PDA can follow to end up in an accept state.
+     * @param string the string to test with this PDA. The string parsing
+     *               begins with at the first character and runs through the
+     *               entire string.
+     * @return True iff the PDA can end up in an accept state after parsing
+     * every character in the string; false otherwise.
+     */
     public boolean accepts(String string) {
-        // TODO: PDA does not deal with epsilon transitions correctly.
         Set<PDAConfiguration> currentConfigs = startConfig();
 
         // currentConfigs.forEach(System.out::println);
